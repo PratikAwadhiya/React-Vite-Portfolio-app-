3118ae5d84392e9ae8dc1014b8bd8bd0993165cb
PratikAwadhiya <pratikawadhiya02@gmail.com>
2024-05-25 11:08:03 +0530

    react+vite portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,77 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate FrontEnd developer with a knack for crafting robust and scalable web applications. With 1 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile Frontend  developer with a passion for creating efficient and user-friendly web applications. With 1 years of professional experience, I have worked with a variety of technologies, including React, Node.js, MySQL, Html,css Js ,Tailwind-css and Angular. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Software Developer",
+    company: "RailWorld",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js,Angular and Figma also. Collaborated with stakeholders to define project requirements and timelines.`,
+    // Implemented RESTful APIs and integrated with MongoDB databases
+    technologies: ["Javascript", "React.js+Vite", "Figma", ],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Curiosity Road @Banglore",
+    description: `Designed and developed user interfaces for web applications using Angular and React-js. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Tailwind-css", "mySQL"],
+  },
+  // {
+  //   year: "2021 - 2022",
+  //   role: "Full Stack Developer",
+  //   company: "Facebook",
+  //   description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+  //   technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  // },
+  // {
+  //   year: "2020 - 2021",
+  //   role: "Software Engineer",
+  //   company: "Paypal",
+  //   description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+  //   technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  // },
+];
+
+export const PROJECTS = [
+  {
+    title: "Merge Quiz",
+    image: project1,
+    description:
+      "A fully functional merge-quiz website with features like merging one time to many quiz , add to more quiz and make a new cart, and user authentication.",
+    technologies: ["HTML", "CSS", "Angular-js", "Firebase",],
+  },
+  {
+    title: "Crystal & Carat",
+    image: project2,
+    description:
+      "Using HTML, CSS, and JavaScript, Crystal & Carat brings this virtual world to life with captivating visuals, smooth animations, and interactive elements. Users can explore the crystal garden, marveling at the intricate designs and vibrant colors of each piece. ",
+    technologies: ["HTML", "CSS", "Angular","java-script"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "Tailwind-css", "React"],
+  },
+  {
+    title: "MLM-Project",
+    image: project4,
+    description:
+      "A platform creating for  multilevel marketing of crystal & carats. Crystal Carat Network is an innovative web platform that combines the allure of precious gemstones with the dynamic structure of Multi-Level Marketing (MLM).",
+    technologies: ["HTML", "CSS", "Angular", "Angular-Materials",],
+  },
+];
+
+export const CONTACT = {
+  address: "Raipur Chhattisgarh,492001",
+  phoneNo: "+91 -- -- -- --",
+  email: "pratikawadhiya02@gmail.com",
+};
